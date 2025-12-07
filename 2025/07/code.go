08282f4a4d29528e251d81e4a4c71e987e63bb25
Dantos7 <daniel.tosone@gmail.com>
2025-12-07 13:36:23 +0100

    🎄 Day 07 - 2nd part

--- 2025/07/code.go
+++ 2025/07/code.go
@@ -42,7 +42,7 @@ func runPart1(input string) any {
 		visited[i] = make([]bool, len(matrix[i]))
 	}
 
-	// Assume position of S is fixed (0, m/2+1)
+	// Assume position of S is fixed (0, m/2)
 	start := [2]int{0, len(matrix[0]) / 2}
 	splits := visitAndCount(matrix, start, visited)
 
@@ -76,6 +76,9 @@ func parseMatrix(input string) [][]rune {
 }
 
 func visitAndCount(matrix [][]rune, start [2]int, visited [][]bool) int {
+	// Perform visit according to rules
+	// Avoid visiting the same branch twice by using the visited matrix
+
 	splits := 0
 	i := start[0] + 1
 	j := start[1]
@@ -108,5 +111,86 @@ func visitAndCount(matrix [][]rune, start [2]int, visited [][]bool) int {
 }
 
 func runPart2(input string) any {
-	return "not implemented"
+	// Strategy:
+	// 	- Visit like in part one
+	//  - From each visited location in the bottom, count the paths from a previous splitter
+	//  - Proceed recursively (use caching to avoid double computation)
+	// Complexity: O(2^(n+1)) time
+	matrix := parseMatrix(input) // Size: n*m
+
+	visited := make([][]bool, len(matrix))
+	for i := range matrix {
+		visited[i] = make([]bool, len(matrix[i]))
+	}
+
+	// use cache to avoid re-computing stuff
+	cache := make([][]int, len(matrix))
+	for i := range matrix {
+		cache[i] = make([]int, len(matrix[i]))
+		for j := range cache[i] {
+			cache[i][j] = -1
+		}
+	}
+
+	// Assume position of S is fixed (0, m/2)
+	start := [2]int{0, len(matrix[0]) / 2}
+	visitAndCount(matrix, start, visited)
+
+	// For visualization purposes - Print visited
+	// for i, line := range visited {
+	// 	for j, v := range line {
+	// 		c := matrix[i][j]
+	// 		if v && c == '.' {
+	// 			fmt.Print("|")
+	// 		} else {
+	// 			fmt.Print(string(c))
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	totalPaths := countPaths(matrix, visited, cache)
+
+	return totalPaths
+}
+
+func countPaths(matrix [][]rune, visited [][]bool, cache [][]int) int {
+	// Non-recursive function that iterates over the last line and calls the recursive function to compute paths
+	lastLineVisited := visited[len(visited)-1]
+	totalPaths := 0
+	for i, v := range lastLineVisited {
+		if v {
+			totalPaths += countEndPaths(matrix, visited, [2]int{len(visited) - 1, i}, cache)
+		}
+	}
+	return totalPaths
+}
+
+func countEndPaths(matrix [][]rune, visited [][]bool, end [2]int, cache [][]int) int {
+	// Recursive function counting from a single end the paths to it
+	if cache[end[0]][end[1]] != -1 {
+		return cache[end[0]][end[1]]
+	}
+	i := end[0] - 1
+	j := end[1]
+	paths := 0
+	for i >= 0 {
+		if visited[i][j] {
+			if j < len(matrix[0])-1 && matrix[i][j+1] == '^' && visited[i-1][j+1] {
+				paths += 1 * countEndPaths(matrix, visited, [2]int{i - 1, j + 1}, cache)
+			}
+			if j > 0 && matrix[i][j-1] == '^' && visited[i-1][j-1] {
+				paths += 1 * countEndPaths(matrix, visited, [2]int{i - 1, j - 1}, cache)
+			}
+		} else if matrix[i][j] == 'S' {
+			paths += 1
+			break
+		} else {
+			break
+		}
+		i--
+	}
+
+	cache[end[0]][end[1]] = paths
+	return paths
 }
